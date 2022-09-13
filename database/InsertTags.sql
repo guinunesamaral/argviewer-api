b96fb05b42145caaa5ee82a44db075f330c2aff9
Guilherme Nunes Amaral dos Santos <guilherme.santos@vikservices.com.br>
2022-09-13 08:49:38 -0300

    commit

--- database/InsertTags.sql
+++ database/InsertTags.sql
@@ -4,4 +4,5 @@ values
 (2, 'Religião', 'Discussões teológicas sobre a existência de Deus e sobre as diferentes religiões existentes.'),
 (3, 'Política', 'Debates sobre as ações de qualquer político ou candidato.'),
 (4, 'Porte de armas', 'As pessoas devem ter o direito de portar armas?'),
-(5, 'Tecnologia', 'Inovações tecnológicas e novidades sobre computadores, consoles, carros, engenharia etc.')
+(5, 'Tecnologia', 'Inovações tecnológicas e novidades sobre computadores, consoles, carros, engenharia etc.'),
+(6, 'Programação', 'Assuntos sobre desenvolvimento de software.'),
