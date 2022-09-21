4e3afad4c686867949bbcfbbe9d887a7c6cf5373
Guilherme Nunes Amaral dos Santos <guilherme.santos@vikservices.com.br>
2022-09-21 15:12:29 -0300

    Update security config

--- src/main/java/com/argviewer/configuration/WebSecurityConfig.java
+++ src/main/java/com/argviewer/configuration/WebSecurityConfig.java
@@ -19,10 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll();
+                .cors();
     }
 
     @Override
